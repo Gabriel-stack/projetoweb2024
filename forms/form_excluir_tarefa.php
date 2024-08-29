4d9ae5712c9d8d2c8e85cf25a98bff294a863f45
Gabriel Alves <gabriel.alvesmartins98@outlook.com>
2024-08-29 13:47:19 -0300

    Atualizar página de exclusão de tarefa e adicionar verificação de usuário logado

--- forms/form_excluir_tarefa.php
+++ forms/form_excluir_tarefa.php
@@ -9,14 +9,22 @@ require_once "../configs/init.php";
 require_once "../database/tarefa.php";
 
 // Verifica se o campo id foi preenchido, senão, retorna mensagem de erro e redireciona para a página anterior.
-
+if(empty($_POST['id'])){
+	setMensagemInstantanea("erro", "Erro ao excluir tarefa: id não informado.");
+	irParaURL(PAGE_HOME);
+}
 // resgata o valor do campo id da tarefa do formulário de excluir tarefa e faz a sanitização dos dados
-
+$id = $_POST['id'];
 //resgata a tarefa pelo id informado no formulário de excluir tarefa
-
+$tarefa = getTarefaPorId($id);
 // se a tarefa não for encontrada, retorna mensagem de erro e redireciona para a página anterior
-
+if($tarefa == false){
+	setMensagemInstantanea("erro", "Erro ao excluir tarefa: tarefa não encontrada.");
+	irParaURL(PAGE_HOME);
+}
 
 // tenta excluir a tarefa no banco de dados, se der certo, adiciona mensagem de sucesso, senão, adiciona mensagem de erro
-
+excluirTarefa($id);
 // redireciona para a página de lista de tarefas (index.php) com a mensagem de sucesso ou erro
+setMensagemInstantanea("sucesso", "Tarefa excluída com sucesso.");
+irParaURL(PAGE_HOME);	
--- forms/logout.php
+++ forms/logout.php
@@ -1,5 +1,7 @@
 <?php
 // arquivo para saida do usuário do sistema
 // importa o arquivo init.php para inicializar a aplicação com as configurações
-
+require_once "../configs/init.php";
 // sai do sistema e redireciona para a página de login
+logout();
+irParaURL(PAGE_LOGIN);
